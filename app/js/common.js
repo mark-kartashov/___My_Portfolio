e7aaaf464c0270c0ce3f6ba6f47ed01758f6db96
mark-kartashov <kartashov.mark.1998@gmail.com>
2018-08-19 22:29:59 +0300

    commit
    v 0.0.2

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,7 @@
 $(function() {
-
-	// Custom JS
-
+	$(function() {
+		$('.menu_icon').on('click', function() {
+			$(this).closest('.menu').toggleClass('menu_state_open') && $(".main-aside").css("width","50vw");
+		});
+	})
 });
