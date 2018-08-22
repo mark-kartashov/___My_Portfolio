2bc3ab8f823a674c688d7ba970d4ae2dacd8bb2d
mark-kartashov <kartashov.mark.1998@gmail.com>
2018-08-22 18:00:27 +0300

    Commit
    v 0.0.5 ( menu reload )

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,48 @@
 $(function() {
-	$(function() {
-		$('.menu_icon').on('click', function() {
-			$(this).closest('.menu').toggleClass('menu_state_open');
-			$('.main-aside').toggleClass('full-aside');
-		});
-	})
+
+	$('.menu-btn').on('click', function() {
+
+		var elem = $(this),
+		    item = $('.menu__list'),
+		    active = 'is-active',
+		    play = 'menu__list--play',
+		    blur_bl = $('aside'),
+		    blur = 'blur';
+
+		if (  elem.hasClass(active) ) {
+			elem.removeClass(active);
+			$(item.get().reverse()).each(function(i) {
+				var row = $(this);
+					setTimeout(function() {
+						row.removeClass(play);
+				}, 50*i);
+			});
+			blur_bl.removeClass(blur);
+			$(blur_bl.get().reverse()).each(function(e) {
+				var row_1 = $(this);
+					setTimeout(function() {
+						blur_bl.removeClass(blur);
+				}, 50*e);
+			});
+		}
+
+		else {
+			elem.addClass(active);
+			item.each(function(i) {
+				var row = $(this);
+					setTimeout(function() {
+						row.addClass(play);
+				}, 50*i);
+			});
+			blur_bl.addClass(blur);
+			$(blur_bl.get().reverse()).each(function(e) {
+				var row_1 = $(this);
+					setTimeout(function() {
+						blur_bl.addClass(blur);
+				}, 50*e);
+			});
+		}
+
+	});
+	
 });
